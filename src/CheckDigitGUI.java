0d389096c011b98979c85272ed7139ef066798e1
Alexandre Klafke <alexandre.m.klafke@gmail.com>
2020-04-16 10:07:34 -0300

    Update form

--- src/CheckDigitGUI.java
+++ src/CheckDigitGUI.java
@@ -1,2 +1,20 @@
-public class CheckDigitGUI {
+import javax.swing.*;
+
+public class CheckDigitGUI extends JFrame{
+    private JPanel mainPainel;
+    private JLabel codeField;
+
+    public CheckDigitGUI (String title){
+        super (title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPainel);
+        this.setResizable(false);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new CheckDigitGUI("Calculadora de digito vérificador");
+        frame.setVisible(true);
+    }
 }
